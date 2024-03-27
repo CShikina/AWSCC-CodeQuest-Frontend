8eb7a51a443288698d32699fa282dba7fa7f15ac
Shikina Cabral <130032168+CShikina@users.noreply.github.com>
2024-03-27 19:27:30 +0800

    DOM

--- frontend-lessons/javascript-tasks/day-20/index.js
+++ frontend-lessons/javascript-tasks/day-20/index.js
@@ -1,39 +1,38 @@
 // JavaScript Fundamentals - Day 20: Assignment
 
-// Task 1: Selecting Elements
+//Task 1: Selecting Elements
+const paragraphs = document.getElementsByTagName("p");
+const infoElements = document.getElementsByClassName("info");
+const headerElement = document.getElementById('header');
+const highlightElement = document.querySelector('.highlight');
 
-// 1. Create an HTML document with various elements, including headings, paragraphs, and divs.
-// 2. Use JavaScript to select and log the following elements to the console:
+console.log(paragraphs);
+console.log(infoElements);
+console.log(headerElement);
+console.log(highlightElement)
 
-//    a. All paragraphs on the page using `getElementsByTagName`.
-   
-//    b. All elements with the class "info" using `getElementsByClassName`.
-   
-//    c. The element with the ID "header" using `getElementById`.
-   
-//    d. The first element with the class "highlight" using `querySelector`.
-
-// Task 2: Modifying Elements
-
-// 3. Change the text content of the second paragraph to "This paragraph is now updated!".
-// 4. Change the background color of the div with the ID "header" to a different color of your choice.
+//Task 2: Modifying Elements
+infoElements.textContent = "This paragraph is now updated!";
+headerElement.style.backgroundColor = "pink";
 
 // Task 3: Creating and Appending Elements
-
-// 5. Create a new `h3` element with the text "New Section" using `document.createElement`.
-// 6. Append the newly created `h3` element to the end of the body of the HTML document.
+const subHeader = document.createElement("h3");
+subHeader.innerText = "New Content";
+document.body.appendChild(subHeader);
+console.log(subHeader)
 
 // Task 4: Removing Elements
-
-// 7. Remove the first paragraph from the document.
+highlightElement.remove();
 
 // Task 5: Handling Events
-
-// 8. Add a click event listener to the div with the ID "header" that logs "Header clicked!" to the console when clicked.
+headerElement.addEventListener("click", onClick);
+function onClick(){
+    alert("Header clicked!");
+}
 
 // Task 6: Advanced Element Manipulation
-
-// 9. Create an array of colors.
-// 10. Use a loop to iterate over all paragraphs and assign a different background color from the array to each paragraph.
-
-// *These tasks will help you practice selecting, modifying, creating, and removing elements from the DOM using JavaScript. Good luck! 🌐*
+let color = ['lightgreen', 'lightblue'];
+console.log(paragraphs.length);
+for (let i=0; i<paragraphs.length; i++){
+    paragraphs[i].style.backgroundColor = color[i];
+}
