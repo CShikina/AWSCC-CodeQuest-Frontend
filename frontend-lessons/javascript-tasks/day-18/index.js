b542aa1ff09f7fdddbaffb35b157ebb27c9ba28e
Shikina Cabral <130032168+CShikina@users.noreply.github.com>
2024-03-27 13:43:54 +0800

    modifying-objects

--- frontend-lessons/javascript-tasks/day-18/index.js
+++ frontend-lessons/javascript-tasks/day-18/index.js
@@ -1,28 +1,49 @@
 // Working with Objects in JavaScript - Day 18: Assignment
 
-// Task 1: Object Basics
+//Task 1
+const book = {
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    year: 2008,
+};
+console.log(book.author);
 
-// 1. Create an empty object named `book`.
-// 2. Add properties to the `book` object for `title`, `author`, and `year` with appropriate values.
-// 3. Access and print the `author` property of the `book` object.
+//Task 2
+const student = {
+    name: 'Shikina',
+    age: 19,
+    grade: 1.00,
+};
+student.age = 20;
+student.subjects = ['Math', 'Science', 'English'];
+console.log(student.subjects);
 
-// Task 2: Object Operations
+//Task 3
+const rectangle = {
+    width: 50,
+    height: 50,
+    calculateArea: function (a, b){
+        return a * b;
+    },
+};
+const area = rectangle.calculateArea(rectangle.width, rectangle.height);
+console.log(area);
 
-// 4. Create an object named `student` with properties for `name`, `age`, and `grade`.
-// 5. Modify the `age` property of the `student` object to a different value.
-// 6. Add a new property named `subjects` to the `student` object, which should be an array of at least three subjects.
+//Task 4
+const person = {
+    name: 'Mark',
+    age: 20,
+    address: 'Manila',
+};
+delete person.address;
 
-// Task 3: Object Methods
+const employee = {
+    name: 'Mark',
+    position: 'Engineer',
+}
 
-// 7. Create an object named `rectangle` with properties for `width` and `height`.
-// 8. Add a method named `calculateArea` to the `rectangle` object that returns the area of the rectangle.
-// 9. Invoke the `calculateArea` method and store the result in a variable, then print it to the console.
-
-// Task 4: Advanced Object Techniques
-
-// 10. Create an object named `person` with properties for `name`, `age`, and `address`.
-// 11. Use the `delete` keyword to remove the `address` property from the `person` object.
-// 12. Create another object named `employee` with properties for `name` and `position`. Combine the `person` and `employee` objects into a new object named `employeeDetails`.
-
-// These tasks will enhance your understanding of working with objects in JavaScript. Objects are versatile and allow you to structure data in a meaningful way. Best of luck! 🌟
-
+const employeeDetails = {
+    ...person,
+    ...employee,
+};
+console.log(employeeDetails);
